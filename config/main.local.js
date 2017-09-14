30f3c26ca3865f6eb6745e4160087e87429fa904
Niraj Kr Chauhan <niraj.1991@gmail.com>
2017-09-14 10:08:34 +0530

    production mongo file added

--- config/main.local.js
+++ config/main.local.js
@@ -2,7 +2,7 @@ module.exports = {
     // Secret key for JWT signing and encryption
     'secret': 'super secret passphrase',
     // Database connection information
-    'database': 'mongodb://pipyala:pippeep@ds129374.mlab.com:29374/pipyalah',
+    'database': 'mongodb://carpooladmin:Welcome5@localhost:27017/carpool',
     // Setting port for server
     'port': process.env.PORT || 3000
 }
